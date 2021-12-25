aafc664baa139d257072334ab20e4b0958d98a86
anilalapati <anil.alapati@ibm.com>
2021-12-25 20:25:07 +0530

    adding another images

--- pages/Ninja/[id].js
+++ pages/Ninja/[id].js
@@ -50,6 +50,9 @@ function Ninja({user}) {
            <h1>Comapany Name:{user.company.name}</h1>  
            <Image src="/28su9mp.jpg" height={500} width={1250}/>
            <Image src="/28su9mp.jpg" height={500} width={1250}/>
+           <Image src="/28su9mp.jpg" height={500} width={1250}/>
+           <Image src="/28su9mp.jpg" height={500} width={1250}/>
+
 
 
             
